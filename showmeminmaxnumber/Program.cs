2ac4e577d23bad8ecb1349a369f36ad7fc766df8
Nayoung <s2018s32@e-mirim.hs.kr>
2020-05-12 23:16:35 +0900

    수 5개 입력, 가장 (작은/큰) 수 구하기

--- showmeminmaxnumber/Program.cs
+++ showmeminmaxnumber/Program.cs
@@ -10,6 +10,22 @@ namespace showmeminmaxnumber
     {
         static void Main(string[] args)
         {
+            int max = 0, min = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("숫자를 입력해주세요 : ");
+                int input = int.Parse(Console.ReadLine());
+                if (i == 0)
+                {
+                    max = input;
+                    min = input;
+                }
+                if (input > max) max = input;
+                if (input < min) min = input;
+            }
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("가장 큰 수 : " + max);
+            Console.WriteLine("가장 작은 수 : " + min);
         }
     }
 }
